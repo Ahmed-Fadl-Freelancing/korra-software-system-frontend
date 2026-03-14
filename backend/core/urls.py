e91b022bb6ec3abe96966f356143de3eeb8d6823
Ahmed Fadl <ahmedfadl3019@gmail.com>
2026-03-14 18:04:21 +0200

    Revert "Refactor backend to local‑RLs"
    This reverts commit 463851a1b9cc9eb971d48c70461d37cf2e33c244, reversing
    changes made to 248a9789b125c628ca77e0674a8e883d9e499baf.

--- backend/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-
-urlpatterns = [
-    path("", include("accounts.urls")),
-    path("", include("workflow.urls")),
-    path("", include("opportunities.urls")),
-    path("", include("documents.urls")),
-    path("", include("rpa.urls")),
-]
--- backend/rpa/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from rpa import views
-
-urlpatterns = [
-    path("webhooks/uipath", views.uipath_webhook),
-]
